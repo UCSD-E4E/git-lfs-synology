6fea1c79d06d853ec4c2a4c56352efb11d5f6546
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-11-19 13:20:37 -0800

    feat: additional logging

--- src/config_dir.rs
+++ src/config_dir.rs
@@ -2,7 +2,9 @@ use std::{fs::create_dir_all, path::PathBuf};
 
 use anyhow::Result;
 use app_dirs2::{AppDataType, AppInfo, app_root};
+use tracing::debug;
 
+#[tracing::instrument]
 pub fn get_config_dir() -> Result<PathBuf> {
     let path = app_root(AppDataType::UserConfig, &AppInfo{
         name: "git-lfs-synology",
@@ -10,6 +12,7 @@ pub fn get_config_dir() -> Result<PathBuf> {
     })?;
 
     if !path.exists() {
+        debug!("Config directory does not exist, creating it.");
         create_dir_all(&path)?;
     }
     
