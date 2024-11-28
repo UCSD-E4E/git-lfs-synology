46cd4e1b60bc2295f1cd27412e4656909be109c2
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-11-27 18:05:42 -0800

    feat: read from git config instead of yaml config

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Development Dependencies
+```bash
+brew install cmake
+```
+
+## Setup a Repository
+
 ```bash
 brew install git-lfs
 ```
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,9 @@
 use std::{fs::read_to_string, path::PathBuf};
 
-use anyhow::Result;
+use anyhow::{anyhow, bail, Context, Ok, Result};
+use gix_config::File;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Configuration {
@@ -14,10 +16,32 @@ impl Configuration {
     pub fn load() -> Result<Configuration> {
         let mut path = PathBuf::new();
         path.push("./");
-        path.push(".git-lfs-synology.yaml");
+        path.push(".lfsconfig");
 
-        let yaml_string = read_to_string(path)?;
+        let config = File::from_path_no_includes(path, gix_config::Source::Local)?;
+        let section = config.section("lfs", None)?;
 
-        Ok(serde_yml::from_str::<Configuration>(yaml_string.as_str())?)
+        let url = section.value("url").context("Url should be set.")?.to_string();
+        let url = if url.starts_with("filestation-secure://") {
+            Ok(url.replace("filestation-secure", "https"))
+        }
+        else if url.starts_with("filestation://") {
+            Ok(url.replace("filestation", "http"))
+        }
+        else {
+            Err(anyhow!("Url is not set incorrectly."))
+        }?;
+
+        let url_parsed = Url::parse(url.as_str())?;
+
+        let path = url_parsed.path();
+        let nas_url = url.replace(path, "");
+
+        Ok(
+            Configuration {
+                nas_url: nas_url.to_string(),
+                path: path.to_string()
+            }
+        )
     }
 }
